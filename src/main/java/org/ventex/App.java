8d7c349c355678673654423e8d9913e5904430ad
charripaul <39438236+charripaul@users.noreply.github.com>
2020-11-18 21:53:43 -0500

    fixed bugs

--- src/main/java/org/ventex/App.java
+++ src/main/java/org/ventex/App.java
@@ -1,8 +1,11 @@
 package org.ventex;
 
+import java.io.File;
+
 public class App {
     public static void main( String[] args ){
-    	System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe");
+    	String basePath = new File("").getAbsolutePath();
+    	System.setProperty("webdriver.chrome.driver", basePath + "\\resources\\chromedriver.exe");
     	
         Bot bot1 = new Bot();
         bot1.setup("https://www.walmart.com/ip/PlayStation-5-Console/363472942");
--- src/main/java/org/ventex/Bot.java
+++ src/main/java/org/ventex/Bot.java
@@ -1,32 +1,21 @@
 package org.ventex;
 
-import java.io.File;
-import java.io.IOException;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Bot {
+	WebDriver chrome;
+	
 	public void setup(String url) {
 		openBrowser(url);
-		startDriver();
 	}
 	
 	public void openBrowser(String url) {
-		WebDriver chrome = new ChromeDriver();
+		chrome = new ChromeDriver();
         chrome.get(url);
 	}
 	
-	public void startDriver() {
-		String basePath = new File("").getAbsolutePath();
-	    System.out.println(basePath);
-		Runtime rt = Runtime.getRuntime();
-		try {
-			rt.exec(basePath + "\\resources\\chromedriver.exe", null, new File(basePath + "\\resources"));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+	public void quit() {
+		chrome.quit();
 	}
 }
