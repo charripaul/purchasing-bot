41bcd808f7d15fca91ba220706366f57c5507d9f
charripaul <39438236+charripaul@users.noreply.github.com>
2020-11-21 17:02:09 -0500

    test: checking if this was being used by heroku

--- src/main/java/org/ventex/App.java
+++ src/main/java/org/ventex/App.java
@@ -16,7 +16,7 @@ public class App {
 	private static final Logger LOGGER = Logger.getLogger(App.class.getName());
 	
     public static void main( String[] args ){
-    	setBinaryLocation();
+//    	setBinaryLocation();
     	Map<String, Object> config = getConfigData();
     	extractDriver("/chromedriver.exe");
     	
