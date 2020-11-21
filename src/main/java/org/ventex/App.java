24f03bd9938de6cab006f0425011c1ac26f27adf
charripaul <39438236+charripaul@users.noreply.github.com>
2020-11-21 16:51:58 -0500

    added 2nd link back after upgraded dyno

--- src/main/java/org/ventex/App.java
+++ src/main/java/org/ventex/App.java
@@ -22,10 +22,10 @@ public class App {
     	
     	List<String> links = new ArrayList<>();
     	links.add("https://www.amazon.com/gp/product/B08FC5L3RG/ref=ox_sc_saved_title_4?smid=ATVPDKIKX0DER&psc=1");
-//    	links.add("https://www.amazon.com/dp/B08FC6MR62/?coliid=IH90F1G2RL9K5&colid=6DFCB5FA7VGW&psc=0&ref_=lv_ov_lig_dp_it_im");
+    	links.add("https://www.amazon.com/dp/B08FC6MR62/?coliid=IH90F1G2RL9K5&colid=6DFCB5FA7VGW&psc=0&ref_=lv_ov_lig_dp_it_im");
     	
     	//1 bootpool for every link, instance count determines number of bots in botpool
-    	int instanceCount = 2;
+    	int instanceCount = 1;
     	List<BotPool> botPoolList = new ArrayList<>();
 
     	for(int x=0;x<links.size();x++) {
